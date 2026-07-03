fe267b9446a4e65cf8a84733eb3a764ca2054184
Abdul Rehman Memon <amemon.bscs22seecs@seecs.edu.pk>
2026-07-03 20:24:52 +0500

    task skeleton

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/abdrehman6224/orchestrator
+
+go 1.26.2
---
+++ task/task.go
@@ -0,0 +1,32 @@
+package task
+
+import (
+	"time"
+
+	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
+)
+
+type State int
+
+const (
+	Pending State = iota
+	Scheduled
+	Running
+	Completed
+	Failed
+)
+
+type Task struct {
+	ID            uuid.UUID
+	Name          string
+	State         State
+	Image         string
+	Memory        int
+	Disk          int
+	ExposedPorts  nat.PortSet
+	PortBinidngs  map[string]string
+	RestartPolicy string
+	StartTime     time.Time
+	EndTime       time.Time
+}
